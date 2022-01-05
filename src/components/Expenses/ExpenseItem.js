d1269d6a692735669764db134361fb3d4c0cc9c9
Dimas Nurfauzi <dimasantafie@gmail.com>
2022-01-05 22:12:34 +0700

    onclick handler and use state

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,9 +1,17 @@
-import { Card } from '../UI/Card';
-import { ExpenseDate } from './ExpenseDate';
-import './ExpenseItem.css';
+import { useState } from "react";
+import { Card } from "../UI/Card";
+import { ExpenseDate } from "./ExpenseDate";
+import "./ExpenseItem.css";
 
 export const ExpenseItem = (props) => {
   const { title, amount, date } = props;
+
+  const [expenseTitle, setExpenseTitle] = useState(title);
+
+  const handleClick = () => {
+    setExpenseTitle("New title");
+    console.log("CLICK!!!");
+  };
 
   return (
     <Card className="expense-item">
@@ -11,9 +19,10 @@ export const ExpenseItem = (props) => {
         <ExpenseDate date={date} />
       </div>
       <div className="expense-item_description">
-        <h2>{title}</h2>
+        <h2>{expenseTitle}</h2>
         <div className="expense-item__price">${amount}</div>
       </div>
+      <button onClick={handleClick}>Change Title</button>
     </Card>
   );
 };
