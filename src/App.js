08b5da51cb1f6ef1ddbbedd7088ef37ca61cf64a
Dimas Nurfauzi <dimasantafie@gmail.com>
2022-01-05 22:22:22 +0700

    create new expense form

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
-import { Expenses } from './components/Expenses/Expenses';
+import { Expenses } from "./components/Expenses/Expenses";
+import { NewExpense } from "./components/NewExpense/NewExpense";
 
 export const App = () => {
   const expenses = [
     {
-      title: 'Car Insurance',
+      title: "Car Insurance",
       amount: 291.41,
       date: new Date(2021, 11, 21),
     },
     {
-      title: 'New TV',
+      title: "New TV",
       amount: 311.41,
       date: new Date(2021, 11, 16),
     },
     {
-      title: 'Wooden Desk',
+      title: "Wooden Desk",
       amount: 451.42,
       date: new Date(2021, 10, 29),
     },
@@ -21,7 +22,7 @@ export const App = () => {
 
   return (
     <div>
-      <h2>Lets Get Started</h2>
+      <NewExpense />
       <Expenses items={expenses} />
     </div>
   );
